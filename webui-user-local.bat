107c9732c881e9d6edad97f3a2cfe285e63c8b9c
Jerry Hogsett <jhogsett@gmail.com>
2023-09-12 20:57:44 -0700

    Update webui-user-local.bat

--- webui-user-local.bat
+++ webui-user-local.bat
@@ -4,9 +4,7 @@ set PYTHON=
 set GIT=
 set VENV_DIR=
 
-rem set COMMANDLINE_ARGS=--no-half --enable-insecure-extension-access --listen --autolaunch --theme dark --ui-config-file ui-config-local.json --ui-settings-file config-local.json
-
-set COMMANDLINE_ARGS=--xformers --enable-insecure-extension-access --listen --autolaunch --theme dark --ui-config-file ui-config-local.json --ui-settings-file config-local.json
+set COMMANDLINE_ARGS=--no-half-vae --xformers --enable-insecure-extension-access --listen --autolaunch --theme dark --ui-config-file ui-config-local.json --ui-settings-file config-local.json
 
 rem set COMMANDLINE_ARGS=--no-half --xformers --administrator --enable-insecure-extension-access --listen --autolaunch --hide-ui-dir-config --theme dark --ui-config-file ui-config-local.json
 
