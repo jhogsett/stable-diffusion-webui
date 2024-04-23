56643c84669f76f460d3d33df50797d41f2a162a
Jerry Hogsett <jhogsett@gmail.com>
2024-04-23 09:30:19 -0700

    updates after 1.9.3 works

--- webui-user-local.bat
+++ webui-user-local.bat
@@ -3,9 +3,6 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-
-set COMMANDLINE_ARGS=--no-half-vae --xformers --enable-insecure-extension-access --listen --autolaunch --theme dark --ui-config-file ui-config-local.json --ui-settings-file config-local.json
-
-rem set COMMANDLINE_ARGS=--no-half --xformers --administrator --enable-insecure-extension-access --listen --autolaunch --hide-ui-dir-config --theme dark --ui-config-file ui-config-local.json
+set COMMANDLINE_ARGS=--no-half-vae --xformers --enable-insecure-extension-access --listen --autolaunch --theme dark
 
 call webui.bat
--- webui-user.bat
+++ webui-user.bat
@@ -4,5 +4,7 @@ set PYTHON=
 set GIT=
 set VENV_DIR=
 set COMMANDLINE_ARGS=
+rem set COMMANDLINE_ARGS=--reinstall-torch --reinstall-xformers --xformers --skip-torch-cuda-test  
+rem set TORCH_COMMAND=install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
 
 call webui.bat
