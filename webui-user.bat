45c72d166658c23e2c0f5fdacd16b6539a53f4fe
Jerry Hogsett <jhogsett@gmail.com>
2024-04-26 13:08:08 -0700

    fix

--- webui-user.bat
+++ webui-user.bat
@@ -3,8 +3,7 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
-rem set COMMANDLINE_ARGS=--reinstall-torch --reinstall-xformers --xformers --skip-torch-cuda-test  
-rem set TORCH_COMMAND=install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
+set COMMANDLINE_ARGS=--reinstall-torch --reinstall-xformers --xformers --skip-torch-cuda-test  
+set TORCH_COMMAND=pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
 
 call webui.bat
